14c1977b6fb0b98ab98be754175c8a3362734d49
vayne <miguel.osorio19@yahoo.com>
2022-02-28 03:11:39 -0600

    testing typewriter effect on text

---
+++ assets/javascript/script.js
@@ -0,0 +1,13 @@
+var i = 0
+var txt = "Hello! My name is Miguel and I make Websites.";
+var speed = 70;
+
+function typewriter() {
+    if (i < txt.length) {
+        document.getElementById('greeting').innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter, speed);
+    }
+}
+
+typewriter();
