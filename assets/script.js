d0fc18d813e107471f70184f403c2502a5434741
vayne <miguel.osorio19@yahoo.com>
2022-02-28 17:09:25 -0600

    setting up the click functions for the slide

--- assets/script.js
+++ assets/script.js
@@ -1,14 +1,21 @@
-var i = 0
-var txt = "Hello! My name is Miguel and I make Websites...";
-var speed = 80;
+// var i = 0
+// var txt = "Hello! My name is Miguel and I make Websites...";
+// var speed = 80;
 
-function typewriter() {
-    if (i < txt.length) {
-        document.getElementById('greeting').innerHTML += txt.charAt(i);
-        i++;
-        setTimeout(typewriter, speed);
-    }
-}
+// function typewriter() {
+//     if (i < txt.length) {
+//         document.getElementById('greeting').innerHTML += txt.charAt(i);
+//         i++;
+//         setTimeout(typewriter, speed);
+//     }
+// }
 
-typewriter();
+// typewriter();
 
+let slidePosition = 0;
+const slides = document.getElementsByClassName('carousel-item');
+const totalSlides = slides.length;
+
+document.getElementById('right-button').addEventListener('click', function () {
+    console.log('hello');
+});
