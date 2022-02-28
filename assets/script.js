ad6122453ff94fe6fef089fa2fd4a5c5ab708dd1
vayne <miguel.osorio19@yahoo.com>
2022-02-28 17:39:17 -0600

    fixing carousel function, renamed carousel divs

--- assets/script.js
+++ assets/script.js
@@ -17,5 +17,40 @@ const slides = document.getElementsByClassName('carousel-item');
 const totalSlides = slides.length;
 
 document.getElementById('right-button').addEventListener('click', function () {
-    console.log('hello');
+    nextSlide();
 });
+
+document.getElementById('left-button').addEventListener('click', function () {
+    prevSlide();
+});
+
+
+function updateSlidePosition() {
+    for (let slide of slides) {
+        console.log(slide);
+        slide.classList.remove('carousel-item-visible');
+        slide.classList.add('carousel-item-hidden');
+    }
+
+    slides[slidePosition].classList.add('carousel-item-visible');
+}
+
+function nextSlide() {
+    console.log('next');
+    if(slidePosition === totalSlides - 1) {
+        slidePosition = 0;
+    } else {
+        slidePosition++;
+    }
+    updateSlidePosition();
+}
+
+function prevSlide() {
+    console.log('back');
+    if(slidePosition === 0) {
+        slidePosition = totalSlides - 1;
+    } else {
+        slidePosition--;
+    }
+    updateSlidePosition();
+}
